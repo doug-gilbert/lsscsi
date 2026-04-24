349ad7cea1d6c31c9491911220801de9306ff010
dougg <dougg@aaa919c1-9816-4d37-901d-0171dbb97e96>
2026-04-24 23:09:43 +0000

    get cmake to publish ls_name_value and ls_name_value_rd
    git-svn-id: https://svn.bingwo.ca/repos/lsscsi/trunk@208 aaa919c1-9816-4d37-901d-0171dbb97e96

--- cmake_del_artifacts.sh
+++ cmake_del_artifacts.sh
@@ -6,10 +6,15 @@ rm -rf \
 	CPackConfig.cmake \
 	CPackSourceConfig.cmake \
 	CMakeFiles \
+	_CPack_Packages \
 	cmake_install.cmake \
+	CTestTestfile.cmake \
+	DartConfiguration.tcl \
 	install_manifest.txt \
+	ls_name_value_rd \
 	lsscsi \
 	lsscsi.8.gz \
 	lsscsi_json.8.gz \
+	Testing \
 	Makefile
 
